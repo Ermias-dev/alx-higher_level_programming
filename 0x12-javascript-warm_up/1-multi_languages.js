af758157e97087f0547907e787b02365094e9ff7
Stacey-bee <staceynakanwagi@gmail.com>
2021-12-15 22:35:18 +0300

    a script that prints 3 lines

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,3 @@
 #!/usr/bin/node
-console.log('C is fun');
-console.log('Python is cool');
-console.log('JavaScript is amazing');
+const myVar = "\n C is fun\n Python is cool\n JavaScript is amazing\n";
+console.log(myVar);
