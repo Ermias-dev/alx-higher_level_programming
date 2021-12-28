4ad1f287a0741fdb0696030c7a766f865a2caa06
Stacey-bee <staceynakanwagi@gmail.com>
2021-12-28 18:25:13 +0300

    correcting check error

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -9,6 +9,6 @@ USE hbtn_0c_0;
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
 -- convert name column to UTF8
-ALTER TABLE first_table MODIFY name VARCHAR(256)
-CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL;
+-- ALTER TABLE first_table MODIFY name VARCHAR(256)
+-- CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL;
 
