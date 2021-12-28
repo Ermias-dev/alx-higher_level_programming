b3289f5480b1641600e281496fec5b64327b7b45
Stacey-bee <staceynakanwagi@gmail.com>
2021-12-28 18:22:51 +0300

    correcting check error

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,5 +1,14 @@
 -- a script that converts database hbtn_0c_0, first_table, and 
 -- field name in first_table to UTF8
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8 COLLATE utf8_general_ci;
-ALTER TABLE first_table CONVERT TO CHARACTER SET utf8;
-ALTER TABLE name COLLATE utf8_general_ci;
+
+-- convert database to UTF8
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- use the database
+USE hbtn_0c_0;
+-- convert first_table to UTF8
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
+-- convert name column to UTF8
+ALTER TABLE first_table MODIFY name VARCHAR(256)
+CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL;
+
