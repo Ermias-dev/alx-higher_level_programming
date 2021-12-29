713df69c9ae8924b58deb92f935aee79c9ee6652
Stacey-bee <staceynakanwagi@gmail.com>
2021-12-29 14:38:01 +0300

    a script that creates the database hbtn_0d_2 and the user user_0d_2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,7 +1,9 @@
--- creates the database hbtn_0d_2 and the user user_0d_2.
--- create database hbtn_0d_2
+-- a script that creates the database hbtn_0d_2 and the user user_0d_2
+-- user_0d_2 should has only SELECT privilege in the database hbtn_0d_2
+-- user_0d_2 password is set to user_0d_2_pwd
+-- If the database hbtn_0d_2 already exists, script doesn't fail
+-- If the user user_0d_2 already exists, script doesn't fail
+
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
--- create user
-CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
--- grant select privilege
-GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2 . * TO 'user_0d_2'@'localhost';
