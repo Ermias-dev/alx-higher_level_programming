7a17b9cef917680c58e5efb487ceb282d373eb55
Stacey-bee <staceynakanwagi@gmail.com>
2021-12-29 14:54:55 +0300

    adding term DEFAULT to the ID attribute

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,4 +1,4 @@
 -- a script that creates the table id_not_null
 -- If the table id_not_null already exists, your script doesn't fail
 
-CREATE TABLE IF NOT EXISTS id_not_null(id INT 1, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS id_not_null(id INT DEFAULT 1, name VARCHAR(256));
