f58a01ccad69b69d07e19dbbaa5a95d053248070
Stacey-bee <staceynakanwagi@gmail.com>
2021-12-29 14:23:15 +0300

    adding IF NOT EXISTS to CREATE USER COMMAND

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -2,5 +2,5 @@
 -- with all privileges
 -- if user already exists, script shouldn't fail
 
-CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
