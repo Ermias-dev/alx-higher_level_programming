582a056e05c313e717493be46731bf93fd8a7b95
Stacey-bee <staceynakanwagi@gmail.com>
2021-12-29 11:49:28 +0300

    adding semicolon at the end of statement

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,7 +1,7 @@
 -- impoting a table dumb into hbtn_0c_0 database
 -- writing a script that displays the average temperature (Fahrenheit)
 -- by city ordered by temperature (descending)
-USE hbtn_0c_0
+USE hbtn_0c_0;
 SELECT city, AVG(value) AS average_temperature
 FROM temperatures
 GROUP BY city
